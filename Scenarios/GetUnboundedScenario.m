23aa91db36188b9e11658dc86f350c72681db67e
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-01-17 13:41:36 +0100

    An unbounded scenario.

--- Scenarios/GetUnboundedScenario.m
+++ Scenarios/GetUnboundedScenario.m
@@ -1,13 +1,13 @@
-function setup = GetBoundedScenario()
+function setup = GetUnboundedScenario()
     setup = GetStandardSetup();
 
     setup.constraints = InitConstraintPolynomials(setup);
-    setup.q = [0.4; -0.5];
+    setup.q = [0.0; 1.0];
     setup.min_order = 4;
     setup.max_order = setup.min_order;
 end
 
 function constraints = InitConstraintPolynomials(setup)
     x = setup.vartable;
-    constraints = [1 - x(1)^2 - x(2)^2, x(1) + x(2)^3];
+    constraints = [x(2) - x(1)^2];
 end
