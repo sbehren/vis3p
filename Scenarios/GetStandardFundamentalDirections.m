4e0e61de961071921cb83e45bbddfc63ea840f01
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-01-17 13:06:34 +0100

    Refacotring complete

---
+++ Scenarios/GetStandardFundamentalDirections.m
@@ -0,0 +1,5 @@
+
+function fundamental_directions = GetStandardFundamentalDirections()
+    %fundamental_directions = {[1; 0], [-1; 0], [0; 1], [0; -1]};
+    fundamental_directions = {[0; -1]};
+end
