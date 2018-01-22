86194e14d0c768d4cf28c79bc0d44a55e229af80
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-01-22 11:18:34 +0100

    Refac: Containment in quadratic module

--- SosProgramming/DeclareVariables.m
+++ SosProgramming/DeclareVariables.m
@@ -19,48 +19,58 @@ function [decision_vars, sos_program] = DeclareHyperplaneVariables(setup, sos_pr
 end
 
 function [decision_vars, sos_program] = DeclareConstraintVariables(setup, order, decision_vars, sos_program)
-    sigma_degrees = GetSigmaDegrees(setup, order);
+    sigma_degs = GetSigmaDegrees(setup, order);
 
-    for i = 1:length(sigma_degrees)
-        sigma_degree = sigma_degrees(i);
-        [decision_vars.sigma(i), sos_program] = DeclareDenseSosPoly(setup.vartable, sigma_degree, sos_program);
+    NotifyUser(sigma_degs);
+    for i = 1:length(sigma_degs)
+        sigma_deg = sigma_degs(i);
+        [decision_vars.sigma(i), sos_program] = DeclareDenseSosPoly(setup.vartable, sigma_deg, sos_program);
     end
 
-    NotifyUser(sigma_degree);
+end
 
-    function sigma_degrees = GetSigmaDegrees(setup, order)
-        sigma_degrees = NaN * ones(length(setup.constraints));
-        constraints = setup.constraints;
+function sigma_degs = GetSigmaDegrees(setup, order)
+    sigma_degs = NaN * ones(length(setup.constraints), 1);
+    constraints = setup.constraints;
 
-        for j = 1:length(constraints)
-            g = constraints(j);
-            deg = GetDegree(g);
-            % Let k = truncation order. Then we require deg(sigma_j g_j) <= k.
-            sigma_degrees(j) = RoundDownToEven(order - deg); 
-        end
+    for j = 1:length(constraints)
+        g = constraints(j);
+        g_deg = GetDegree(g);
+        sigma_degs(j) = GetSigmaDegree(g_deg, order);
     end
 end
 
-function [dense_sos_poly, sos_program] = DeclareDenseSosPoly(vartable, degree, sos_program)
-    assert(mod(degree, 2) == 0, 'Degree of sos poly needs to be even.')
-    if degree < 0
-        dense_sos_poly = 0;
-    elseif degree == 0
-        dense_sos_poly = 1;
+function sigma_deg = GetSigmaDegree(g_deg, order)
+    if g_deg > order
+       sigma_deg = -Inf;
     else
-        monomials_vec = monomials( vartable, 0: degree );
-        [sos_program, dense_sos_poly] = sossosvar(sos_program, monomials_vec);
+       % Let k = truncation order. Then we require deg(sigma_j g_j) <= k.
+       sigma_deg = RoundDownToEven(order - g_deg); 
     end
 end
 
-function degree = GetDegree(poly)
-    degree = feval(symengine, 'degree', poly);
+function [dense_sos_poly, sos_program] = DeclareDenseSosPoly(vartable, deg, sos_program)
+    if deg == -Inf
+        dense_sos_poly = 0;
+    elseif deg == 0
+        dense_sos_poly = 1;
+    elseif deg > 0
+        assert(mod(deg, 2) == 0, 'Degree of nontrivial sos poly needs to be even.')
+        monomials_vec = monomials( vartable, 0: deg );
+        [sos_program, dense_sos_poly] = sossosvar(sos_program, monomials_vec);
+    else
+        error('VI: Illegal polynomial degree.');
+    end
+end
+
+function deg = GetDegree(poly)
+    deg = feval(symengine, 'degree', poly);
 end
 
 function result = RoundDownToEven(n)
     result = floor(n/2)*2;
 end
 
-function NotifyUser(sigma_degrees)
-    fprintf('VI: Created sos polynomial of degree(s) %s.', Vec2Str(sigma_degrees));
+function NotifyUser(sigma_degs)
+    fprintf('VI: Created sos polynomial of degree(s) %s.', Vec2Str(sigma_degs));
 end
