88966fead3730f9944a9d0cb7db7d2be2d770ca1
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-01-31 10:16:47 +0100

    Before refactoring scenarios

--- Scenarios/GetStandardSetup.m
+++ Scenarios/GetStandardSetup.m
@@ -5,6 +5,6 @@ function setup = GetStandardSetup()
 end
 
 function fundamental_directions = GetStandardFundamentalDirections()
-    %fundamental_directions = {[1; 0], [-1; 0], [0; 1], [0; -1]};
-    fundamental_directions = {[0; -1]}; %standard test
+    fundamental_directions = {[1; 0], [-1; 0], [0; 1], [0; -1]};
+    %fundamental_directions = {[0; -1]}; %standard test
 end
