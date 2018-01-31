7aa2930b9d92f232eceeb1ea160bd7ef3c0b3e8e
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-01-31 10:42:26 +0100

    Refac: WriteTexfile in separate MATLAB-file. Typos

---
+++ Logging/WriteTexfile.m
@@ -0,0 +1,71 @@
+function WriteTexfile(setup, result)
+    filename = setup.GetTexfileName;
+    file = fopen(filename, 'w');
+    
+    WriteHeader(file, setup);
+
+    WriteConstraints(file, setup);
+    WriteLabel(file);
+    WriteOrder(file, setup);
+
+    WriteOptimalValue(file, result);
+    WriteDirection(file, result);
+    WriteRhs(file, result);
+    WriteFigureLabelAndNewline(file, setup);
+end
+
+function WriteHeader(file, setup)
+    hash = setup.GetFigureHash();
+
+    fwriteln(file, ['% Results for figure with SHA256 = ' hash ' follow in next line.']);
+
+end
+
+function WriteConstraints(file, setup)
+    constraints = setup.GetConstraintStrings();
+    for i = 1:length(constraints)
+        con = ['$' constraints{i} '$'];
+        WriteCell(file, con);
+    end
+end
+
+function WriteLabel(file)
+    WriteCell(file, 'insert label');
+end
+
+function WriteOrder(file, setup)
+    order_str = num2str(setup.truncation_order);
+    WriteCell(file, order_str);
+end
+
+function WriteOptimalValue(file, result)
+    WriteFloatToCell(file, result.objective);
+end
+
+function WriteDirection(file, result)
+    a = result.a;
+    for i = 1:length(a)
+        WriteFloatToCell(file, result.a(i));
+    end
+end
+
+function WriteRhs(file, result)
+    WriteFloatToCell(file, result.b);
+end
+
+function WriteFigureLabelAndNewline(file, setup)
+    name = setup.name;
+    label = ['\ref{fig:' name '} \\'];
+    fwriteln(file, label);
+end
+
+function WriteFloatToCell(file, number)
+    WriteCell(file, Float2ShortString(number));
+end
+function WriteCell(file, str)
+    fwritestr(file, [str ' & ']);
+end
+
+function result = Float2ShortString(number)
+    result = Float2Str(number, 4);
+end
