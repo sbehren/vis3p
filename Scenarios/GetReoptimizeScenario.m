01375cda331906f67058805e2df34108851902e0
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-02-08 10:28:13 +0100

    Scenario reoptimize: Derive from feasibility

--- Scenarios/GetReoptimizeScenario.m
+++ Scenarios/GetReoptimizeScenario.m
@@ -1,15 +1,7 @@
 function scenario = GetReoptimizeScenario()
-    scenario = GetStandardScenario();
+    scenario = GetNoFeasiblePointScenario();
     scenario.name = 'reoptimize';
 
-    scenario.is_feasibility_variant = true;
     scenario.normal_is_fixed = true;
-    scenario.constraints = InitConstraintPolynomials(scenario);
     scenario.q = false;
-    scenario.truncation_order = 5;
-end
-
-function constraints = InitConstraintPolynomials(scenario)
-    x = scenario.vartable;
-    constraints = [x(2) - x(1)^2, x(2) - x(1)];
 end
