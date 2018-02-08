eaaa8fd84c6ca592087cdccbc8a9f3285499c9af
Sönke Behrends <s.behrends@math.uni-goettingen.de>
2018-02-08 15:11:19 +0100

    WriteTexfile: Refactor and use join. Close file at the end.

--- @Scenario/WriteTexfile.m
+++ @Scenario/WriteTexfile.m
@@ -1,22 +1,20 @@
 function WriteTexfile(obj)
-    filename = obj.GetTexfileName;
-    file = fopen(filename, 'w');
-    
-    WriteLatexTable(file, obj);
+    file = OpenTexfile(obj);
+
+    WriteHeaderComment(file, obj);
+    WriteLatexTableCells(file, obj);
+    WriteLineEnding(file);
+
+    CloseTexfile(file)
 end
 
-function WriteLatexTable(file, scenario)
-    WriteHeaderComment(file, scenario);
+function file = OpenTexfile(obj)
+    filename = obj.GetTexfileName;
+    file = fopen(filename, 'w');
+end
 
-    WriteConstraints(file, scenario);
-    WriteFeasiblePoint(file, scenario);
-    WriteProgramLabel(file);
-    WriteOrder(file, scenario);
-
-    WriteOptimalValue(file, scenario);
-    WriteDirection(file, scenario);
-    WriteRhs(file, scenario);
-    WriteFigureLabelAndNewline(file, scenario);
+function CloseTexfile(file)
+    fclose(file);
 end
 
 function WriteHeaderComment(file, scenario)
@@ -26,63 +24,94 @@ function WriteHeaderComment(file, scenario)
     fwriteln(file, ['% Results for figure ' figurename ' with SHA256 = ' hash ' follow in next line.']);
 end
 
-function WriteConstraints(file, scenario)
-    constraints = scenario.GetConstraintStrings();
-    for i = 1:length(constraints)
-        con = ['$' constraints{i} '$'];
-        WriteCell(file, con);
-    end
+function WriteLatexTableCells(file, scenario)
+    cell_values = GetTableCells(scenario);
+    WriteCellsToFile(file, cell_values);
 end
 
-function WriteFeasiblePoint(file, scenario)
-    for i = 1:scenario.num_vars
+function WriteCellsToFile(file, cell_values)
+    cell_string = join(cell_values, ' & ');
+    fwrite(file, cell_string);
+end
+
+function cell_values = GetTableCells(scenario)
+    cell_values = {};
+
+    cell_values          = AppendConstraintStr(cell_values, scenario);
+    cell_values          = AppendFeasiblePointStr(cell_values, scenario);
+
+    cell_values{end + 1} = GetProgramLabel();
+    cell_values{end + 1} = GetOrder(scenario);
+
+    cell_values{end + 1} = GetOptimalValue(scenario);
+    cell_values          = AppendNormalVector(cell_values, scenario);
+    cell_values{end + 1} = GetRhs(scenario);
+
+    cell_values{end + 1} = GetFigureLabel(scenario);
+end
+
+function cell_values = AppendConstraintStr(cell_values, scenario)
+    constraints = scenario.GetConstraintStrings();
+    len = length(constraints);
+    con_cell = cell(1, len);
+    for i = 1:len
+        con_cell{i} = ['$' constraints{i} '$'];
+    end
+    cell_values = [cell_values, con_cell];
+end
+
+function cell_values = AppendFeasiblePointStr(cell_values, scenario)
+    num_cells = scenario.num_vars;
+    q_strs = cell(1, num_cells);
+    for i = 1:num_cells
         if scenario.is_feasibility_variant
             q_str = 'n/a';
         else
             current_coordinate = scenario.q(i);
             q_str = Float2ShortString(current_coordinate);
         end
-        WriteCell(file, q_str);
+        q_strs{i} = q_str;
     end
+    cell_values = [cell_values, q_strs];
 end
 
-function WriteProgramLabel(file)
-    WriteCell(file, 'insert label');
+function result = GetProgramLabel()
+    result = 'insert label';
 end
 
-function WriteOrder(file, scenario)
+function order_str = GetOrder(scenario)
     order_str = num2str(scenario.truncation_order);
-    WriteCell(file, order_str);
 end
 
-function WriteOptimalValue(file, scenario)
-    WriteFloatToCell(file, scenario.objective);
+function result = GetOptimalValue(scenario)
+    result = Float2ShortString(scenario.objective);
 end
 
-function WriteDirection(file, scenario)
+function cell_values = AppendNormalVector(cell_values, scenario)
     a = scenario.a;
-    for i = 1:length(a)
-        WriteFloatToCell(file, scenario.a(i));
+    len = length(a);
+
+    a_str = cell(1, len);
+    for i = 1:len
+        a_str{i} = Float2ShortString(scenario.a(i));
     end
+
+    cell_values = [cell_values, a_str];
 end
 
-function WriteRhs(file, scenario)
-    WriteFloatToCell(file, scenario.b);
+function result = GetRhs(scenario)
+    result = Float2ShortString(scenario.b);
 end
 
-function WriteFigureLabelAndNewline(file, scenario)
+function result = GetFigureLabel(scenario)
     name = scenario.name;
-    label = ['\ref{fig:' name '} \\'];
-    fwriteln(file, label);
-end
-
-function WriteFloatToCell(file, number)
-    WriteCell(file, Float2ShortString(number));
-end
-function WriteCell(file, str)
-    fwritestr(file, [str ' & ']);
+    result = ['\ref{fig:' name '}'];
 end
 
 function result = Float2ShortString(number)
     result = Float2Str(number, 2);
+end
+
+function WriteLineEnding(file)
+    fwrite(file, ' \\');
 end
